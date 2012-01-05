bd0da66dbe975a0d8bbdf40d1d4064e18ddba168
Lauri Harsila <murgo@iki.fi>
2012-01-05 14:53:18 +0200

    Initial, empty version of android client

---
+++ Android/IrssiNotifier/src/fi/iki/murgo/irssinotifier/BroadcastReceiver.java
@@ -0,0 +1,5 @@
+package fi.iki.murgo.irssinotifier;
+
+public class BroadcastReceiver {
+
+}
---
+++ Android/IrssiNotifier/src/fi/iki/murgo/irssinotifier/IrssiNotifierActivity.java
@@ -0,0 +1,13 @@
+package fi.iki.murgo.irssinotifier;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IrssiNotifierActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
