681224983f7c2b2f4054cc50e10d27e047e92e54
Jussi Rajala <zemm@iki.fi>
2012-02-24 01:23:36 +0200

    Added feature for preventing notifications until keyboard idle of given seconds

--- Irssi/irssinotifier.pl
+++ Irssi/irssinotifier.pl
@@ -23,6 +23,8 @@ my $lastNick;
 my $lastAddress;
 my $lastTarget;
 
+my $lastActivity = time;
+
 sub private {
     my ($server, $msg, $nick, $address) = @_;
     $lastMsg = $msg;
@@ -45,9 +47,18 @@ sub print_text {
     my ($dest, $text, $stripped) = @_;
 
     my $opt = MSGLEVEL_HILIGHT|MSGLEVEL_MSGS;
-    if(($dest->{level} & ($opt)) && (($dest->{level} & MSGLEVEL_NOHILIGHT) == 0) && (!Irssi::settings_get_bool("irssinotifier_away_only") || $lastServer->{usermode_away} )) {
+    if (
+        ($dest->{level} & ($opt)) && (($dest->{level} & MSGLEVEL_NOHILIGHT) == 0) &&
+        (!Irssi::settings_get_bool("irssinotifier_away_only") || $lastServer->{usermode_away}) &&
+        activity_allows_hilight()
+    ) {
         hilite();
     }
+}
+
+sub activity_allows_hilight {
+    my $timeout = Irssi::settings_get_int('irssinotifier_require_idle_seconds');
+    return ($timeout <= 0 || (time - $lastActivity) > $timeout);
 }
 
 sub hilite {
@@ -105,10 +116,27 @@ sub decrypt {
     return $result;
 }
 
+sub setup_keypress_handler {
+    Irssi::signal_remove('gui key pressed', 'event_key_pressed');
+    Irssi::print('removed keylistener');
+    if (Irssi::settings_get_int('irssinotifier_require_idle_seconds') > 0) {
+        Irssi::signal_add('gui key pressed', 'event_key_pressed');
+        Irssi::print('registered keylistener');
+    }
+}
+
+sub event_key_pressed {
+    $lastActivity = time;
+}
+
 Irssi::settings_add_str('IrssiNotifier', 'irssinotifier_encryption_password', 'password');
 Irssi::settings_add_str('IrssiNotifier', 'irssinotifier_api_token', '');
 Irssi::settings_add_bool('IrssiNotifier', 'irssinotifier_away_only', false);
+Irssi::settings_add_int('IrssiNotifier', 'irssinotifier_require_idle_seconds', 0);
 
 Irssi::signal_add('message public', 'public');
 Irssi::signal_add('message private', 'private');
 Irssi::signal_add('print text', 'print_text');
+Irssi::signal_add('setup changed', 'setup_keypress_handler');
+
+setup_keypress_handler();
