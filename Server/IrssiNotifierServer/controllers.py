d78634aee49884aa0d29dd3bb6e5caba8f2fec35
Lauri Härsilä <murgo@iki.fi>
2013-06-02 02:52:56 +0300

    Changed latest script version in the web site

--- Server/IrssiNotifierServer/controllers.py
+++ Server/IrssiNotifierServer/controllers.py
@@ -14,7 +14,7 @@ import json
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 MinAndroidVersion = 8
 MinScriptVersion = 2
-LatestScriptVersion = 17
+LatestScriptVersion = 18
 
 
 def getAndroidServerMessage(data):
